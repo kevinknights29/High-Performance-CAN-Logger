1691227ac2313296c408d7d3dc8b3dd6f59e8385
Kevin Knights <kevinknights29@hotmail.com>
2025-05-08 13:00:12 -0500

    feat: added nix shell config

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let 
+    nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+    pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+    packages = with pkgs; [
+        libgcc
+        gnumake
+    ];
+}
