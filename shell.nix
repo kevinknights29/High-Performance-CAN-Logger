5f3009c36f41856f0ef6d0c2d61e0bd89fe46b2c
Kevin Knights <kevinknights29@hotmail.com>
2025-05-09 20:52:52 +0000

    feat: added tmux and script to create a vcan interface

--- shell.nix
+++ shell.nix
@@ -7,5 +7,19 @@ pkgs.mkShell {
     packages = with pkgs; [
         libgcc
         gnumake
+        tmux
     ];
+
+    shellHook = ''
+        # Load virtual CAN module
+        sudo modprobe vcan
+
+        # Create virtual interface
+        sudo ip link add dev vcan0 type vcan
+
+        # Activate interface
+        sudo ip link set up vcan0
+
+        echo "Created VCAN interface 'vcan0'"
+    '';
 }
